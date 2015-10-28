7fbf8f48c93e3f09ff08173199106161fc787e68
neon520 <javierpg520@gmail.com>
2015-10-28 09:21:52 +0100

    Arreglando test

--- appIV/test.py
+++ appIV/test.py
@@ -1,41 +1,41 @@
 import unittest
 import webapp2
-import __main__
+import gestorhtml
 
 class TestStringMethods(unittest.TestCase):
         def test_empresas(self):
             request = webapp2.Request.blank('/empresas')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Empresas registradas' in response.body)
 
         def test_borrarCalificacion(self):
             request = webapp2.Request.blank('/borrarCalificacion')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Borrado de una Calific' in response.body)
 
         def test_calificaciones(self):
             request = webapp2.Request.blank('/calificaciones')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Calificaciones registradas' in response.body)
 
         def test_registroCalificar(self):
             request = webapp2.Request.blank('/registroCalificar')
-            response = request.get_response(main.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Registro de una Calific' in response.body)
 
         def test_registroEmpresa(self):
             request = webapp2.Request.blank('/registroEmpresa')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Registro de la empresa' in response.body)
 
         def test_registroUsuario(self):
             request = webapp2.Request.blank('/registroUsuario')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('Registro de alumno' in response.body)
 
         def test_base(self):
             request = webapp2.Request.blank('/')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             self.assertTrue('n de empresas' in response.body)
 
 if __name__ == '__main__':
--- appIV/test_sure.py
+++ appIV/test_sure.py
@@ -1,47 +1,47 @@
 import sure
 import webapp2
-import __main__
+import gestorhtml
 
 class TestStringMethods:
         def test_empresas(self):
             request = webapp2.Request.blank('/empresas')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Empresas registradas' in response.body)
             (response.body).should.contain('Empresas registradas')
 
         def test_borrarCalificacion(self):
             request = webapp2.Request.blank('/borrarCalificacion')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Borrado de una Calific' in response.body)
             (response.body).should.contain('Borrado de una Calific')
 
         def test_calificaciones(self):
             request = webapp2.Request.blank('/calificaciones')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Calificaciones registradas' in response.body)
             (response.body).should.contain('Calificaciones registradas')
 
         def test_registroCalificar(self):
             request = webapp2.Request.blank('/registroCalificar')
-            response = request.get_response(main.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Registro de una Calific' in response.body)
             (response.body).should.contain('Registro de una Calific')
 
         def test_registroEmpresa(self):
             request = webapp2.Request.blank('/registroEmpresa')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Registro de la empresa' in response.body)
             (response.body).should.contain('Registro de la empresa')
 
         def test_registroUsuario(self):
             request = webapp2.Request.blank('/registroUsuario')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Registro de alumno' in response.body)
             (response.body).should.contain('Registro de alumno')
 
         def test_base(self):
             request = webapp2.Request.blank('/')
-            response = request.get_response(__main__.app)
+            response = request.get_response(gestorhtml.Server())
             #self.assertTrue('n de empresas' in response.body)
             (response.body).should.contain('n de empresas')
 
